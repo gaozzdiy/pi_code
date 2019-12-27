6522a9ff3b1b2010dc8fabcd99610fc778abe50c
gaozzdiy <gaozzdiy@126.com>
2019-12-27 13:01:46 +0000

    adjust some function

--- minex/minex.py
+++ minex/minex.py
@@ -1,9 +1,10 @@
 import getopt, sys
 import time
-import cv2
+import cv2,numpy
 import RPi.GPIO as GPIO
 import threading
 import socket
+import os
 
 LINE_Y = 280
 BR_VAL = 70
@@ -11,6 +12,7 @@ ADJ_MOTO_VAL = 20
 FETCH_LINE = 2
 FETCH_COL = 5
 
+SRVPORT = 8002
 
 MOTOA = 36
 MOTOB = 38
@@ -18,42 +20,38 @@ MOTOB = 38
 RAILA = 35
 RAILB = 37
 
+STAT = "STOP"
 
 #motor driver
 def moto(pos,col):
     left,right = checkborder()
     print("end : left,right=",left,right)
     if pos < col/2 - ADJ_MOTO_VAL and pos > 0 and left == 0:
-        motoleft()
-        #print("moto: right~~~~ pos ,col,ADJ_MOTO_VAL",pos,col,ADJ_MOTO_VAL)
+        moto_mv("left")
         print("moto: left~~~~ pos ,col/2 - ADJ_MOTO_VAL",pos,col/2-ADJ_MOTO_VAL)
     elif pos > col/2 + ADJ_MOTO_VAL and pos < col and right == 0:
-        motoright()
-        #print("moto: left ~~~~ pos ,col,ADJ_MOTO_VAL",pos,col,ADJ_MOTO_VAL)
+        moto_mv("right")
         print("moto: right ~~~~ pos ,col/2 + ADJ_MOTO_VAL",pos,col/2+ADJ_MOTO_VAL)
     elif pos > col/2 - ADJ_MOTO_VAL and pos < col/2 + ADJ_MOTO_VAL:
-        motostop()
+        moto_mv("stop")
         print("moto: in the pos")
     else :
-        motostop()
+        moto_mv("stop")
         print("other unknow status:(left,right,pos)",left,right,pos)
     #time.sleep(0.2)
     return
 
-def motoright():
-    GPIO.output(MOTOB, GPIO.HIGH)
-    GPIO.output(MOTOA, GPIO.LOW)
-    return
 
-def motoleft():
-    GPIO.output(MOTOA, GPIO.HIGH)
-    GPIO.output(MOTOB, GPIO.LOW)
-    return
-
-def motostop():
-    GPIO.output(MOTOB, GPIO.HIGH)
-    GPIO.output(MOTOA, GPIO.HIGH)
-    return
+def moto_mv(action):
+    if action == "left":
+        GPIO.output(MOTOA, GPIO.HIGH)
+        GPIO.output(MOTOB, GPIO.LOW)
+    elif action == "right":
+        GPIO.output(MOTOB, GPIO.HIGH)
+        GPIO.output(MOTOA, GPIO.LOW)
+    else:
+        GPIO.output(MOTOB, GPIO.HIGH)
+        GPIO.output(MOTOA, GPIO.HIGH)
 
 def checkborder():
     left = GPIO.input(RAILB)
@@ -64,8 +62,6 @@ def checkborder():
 #graph trans
 def imgtrans(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.GaussianBlur(img,(5,5),3,3)
-    img = cv2.GaussianBlur(img,(5,5),3,3)
     img = cv2.GaussianBlur(img,(5,5),3,3)
     img = cv2.GaussianBlur(img,(5,5),3,3)
     img = cv2.GaussianBlur(img,(5,5),3,3)
@@ -97,35 +93,90 @@ def getpos(img):
     return pos,col
 
 def rundev():
+    global STAT
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(MOTOA,GPIO.OUT)
     GPIO.setup(MOTOB,GPIO.OUT)
     GPIO.setup(RAILA,GPIO.IN)
     GPIO.setup(RAILB,GPIO.IN)
+    while True:
+        if STAT == "STOP":
+            runReg()
+            print("machine stoped")
+            time.sleep(10)
+        elif STAT == "RUN":
+            try:
+                cma = cv2.VideoCapture(0)
+                while(cma.isOpened()):
+                    cma.read()
+                    ret, img = cma.read()
+                    pos, col = getpos(img)
+                    print("pos, col =",pos,col)
+                    moto(pos,col)
+                    #print(pos,col)
+                    #time.sleep(0.1)
+            except Exception as e:
+                print("rundev except:",e)
+            finally:
+                moto_mv("stop")
+
+
+def runReg():
+    global STAT
+    cpuserial = os.popen('grep Serial /proc/cpuinfo|awk -F\' \' \'{print $NF}\'').read().split('\n')[0]
+    mac = os.popen('ip a|grep ether|awk -F\' \' \'{print $2}\'').read().split('\n')[0]
+    conn = createSock()
+    regval = "cpu," + cpuserial + ",mac," + mac + ",port," + str(SRVPORT)
+    conn.send("regdev".encode('utf-8'))
+    conn.send(regval.encode('utf-8'))
+    ret = conn.recv(1024).decode()
+    if ret == "succ":
+        STAT = "REG"
+    elif ret == "alre":
+        STAT = "REG"
+    elif ret == "fail":
+        STAT = "STOP"
+    conn.send("exit".encode('utf-8'))
+    if conn.recv(1024).decode() == "exit":
+        print("finish dialog!")
+    conn.close()
+
+
+def createSock():
+    address = ('192.168.4.201', 8002)
     try:
-        cma = cv2.VideoCapture(0)
-        while(cma.isOpened()):
-            cma.read()
-            ret, img = cma.read()
-            pos, col = getpos(img)
-            print("pos, col =",pos,col)
-            moto(pos,col)
-            #print(pos,col)
-            #time.sleep(0.1)
-    except Exception as e:
-        print(e)
-    finally:
-        motostop()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(address)
+    except socket.error as msg:
+        print(msg)
+        sys.exit(1)
+    return sock
+
+
+def sendpic(conn):
+    capture = cv2.VideoCapture(0)
+    capture.read()
+    capture.read()
+    ret, frame = capture.read()
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 75]
+    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+    picstrdata = numpy.array(imgencode).tostring()
+    conn.send(str.encode(str(len(picstrdata)).ljust(16)))
+    conn.send(picstrdata)
+    capture.release()
 
 
 def runCommand(conn, command):
-    print(command)
+    if command == "camera":
+        sendpic(conn)
+    elif command == "cfg":
+        print("getcfg")
 
 
 def runServ():
     global STAT
-    address = ('0.0.0.0', 8004)
+    address = ('0.0.0.0', SRVPORT)
     print("init server! SERV=", address)
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serv.bind(address)
@@ -143,7 +194,7 @@ def runServ():
                 elif not newbuf:
                     break
                 else:
-                    run_command(conn, newbuf.decode())
+                    runCommand(conn, newbuf.decode())
                     break
             conn.close()
         except Exception as e:
@@ -163,9 +214,8 @@ def create_thread():
             time.sleep(5)
     except Exception as e:
         print(e)
-        print("have except, exit anyway!")
     finally:
-        motostop()
+        moto_mv("stop")
 
 
 if __name__ == "__main__":
